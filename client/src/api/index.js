14ad9c08b42cc5b836e8a9e59df7d81730ba689c
marshaljr <marshaljr615@gmail.com>
2026-01-09 20:11:11 +0545

    server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
-const url = "http://localhost:5000/posts";
+// const url = "http://localhost:5000/posts";
 
-export const fetchPosts = () => axios.get(url);
+// export const fetchPosts = () => axios.get(url);
+
+const API = axios.create({
+  baseURL: "http://localhost:5000",
+});
+
+export const fetchPosts = () => API.get("/posts");
--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const db = process.env.MONGODB_URI;
 
 mongoose
