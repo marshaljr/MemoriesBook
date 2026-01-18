31b10608a50ad140c1150ba906b0e92f8f4c7240
marshaljr <marshaljr615@gmail.com>
2026-01-18 21:17:36 +0545

    network issue fixed

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-// const url = "http://localhost:5000/posts";
+// const url = "http://localhost:5000";
 
 // export const fetchPosts = () => axios.get(url);
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "http://localhost:5001",
 });
 
 export const fetchPosts = () => API.get("/posts");
--- server/index.js
+++ server/index.js
@@ -8,21 +8,27 @@ dotenv.config();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
-
 app.use("/posts", postRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
 const db = process.env.MONGODB_URI;
 
 mongoose
   .connect(db)
   .then(() =>
     app.listen(PORT, () =>
-      console.log(`🚀 Server running on http://localhost:${PORT}`)
-    )
+      console.log(`🚀 Server running on http://localhost:${PORT}`),
+    ),
   )
   .catch((error) => console.log(error.message));
